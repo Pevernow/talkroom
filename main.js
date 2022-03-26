09af1af2ab73172b613f86a39707265d3888dbe3
Pevernow <3450354617@qq.com>
2022-03-26 09:50:34 +0800

    Update main.js

--- main.js
+++ main.js
@@ -26,15 +26,57 @@ function init() {
 }
 
 function login(name){
-    realtime.createIMClient(name).then(function(user_IMClient) {
+    Realtime.createIMClient(name).then(function(user_IMClient) {
         Client = user_IMClient;
         // 成功登录
         Client.getConversation('CONVERSATION_ID').then(function(conversation) {
-              return conversation.join();
+            return conversation.join();
         }).then(function(conversation) {
-              console.log('加入成功', conversation.members);
-              }).catch(console.error.bind(console));
+            gConversation = conversation
+            console.log('加入成功', conversation.members);
+            Client.on(Event.MESSAGE, onMessageGet);
+            }).catch(console.error.bind(console));
         }).catch(alert);
+}
+
+function sendMessage(message){
+    gConversation.send(new TextMessage('大家好，欢迎来到我们的群聊对话'));
+}
+
+function onMessageGet(message, conversation){
+// 请按自己需求改写
+  var file;
+  switch (message.type) {
+    case TextMessage.TYPE:
+      console.log('收到文本消息，内容：' + message.getText() + '，ID：' + message.id);
+      break;
+    case FileMessage.TYPE:
+      file = message.getFile(); // file 是 AV.File 实例
+      console.log('收到文件消息，URL：' + file.url() + '，大小：' + file.metaData('size'));
+      break;
+    case ImageMessage.TYPE:
+      file = message.getFile();
+      console.log('收到图像消息，URL：' + file.url() + '，宽度：' + file.metaData('width'));
+      break;
+    case AudioMessage.TYPE:
+      file = message.getFile();
+      console.log('收到音频消息，URL：' + file.url() + '，长度：' + file.metaData('duration'));
+      break;
+    case VideoMessage.TYPE:
+      file = message.getFile();
+      console.log('收到视频消息，URL：' + file.url() + '，长度：' + file.metaData('duration'));
+      break;
+    case LocationMessage.TYPE:
+      var location = message.getLocation();
+      console.log('收到位置消息，纬度：' + location.latitude + '，经度：' + location.longitude);
+      break;
+    case 1:
+      console.log('OperationMessage 是自定义消息类型');
+    default:
+      // 未来可能添加新的自定义消息类型，新版 SDK 也可能添加新的消息类型。
+      // 因此别忘了在默认分支中处理未知类型，例如提示用户升级客户端至最新版本。
+      console.warn('收到未知类型消息');
+  }
 }
 
 window.addEventListener("load", function () {
